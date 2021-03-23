3387461344cf83fff2f4b98f043733a53e30ffb4
goandreus <andres.chavez.crisostomo@gmail.com>
2021-03-23 16:16:01 -0500

    usuario mode (#7)

--- lib/models/usuario.dart
+++ lib/models/usuario.dart
@@ -1,10 +1,14 @@
-import 'package:meta/meta.dart';
-
 class Usuario {
-  String nombre;
-  int edad;
-  List<String> profesiones;
+  final String nombre;
+  final int edad;
+  final List<String> profesiones;
 
-  Usuario({@required this.nombre, this.edad, this.profesiones})
-      : assert(nombre != null);
+  Usuario({this.nombre, this.edad, this.profesiones});
+
+  Usuario copyWith({String nombre, int edad, List<String> profesiones}) =>
+      Usuario(
+        nombre: nombre ?? this.nombre,
+        edad: edad ?? this.edad,
+        profesiones: profesiones ?? this.profesiones,
+      );
 }
