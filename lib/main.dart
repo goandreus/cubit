de2a4295dc3d1f06dba27a30915d560922e70180
goandreus <andres.chavez.crisostomo@gmail.com>
2021-03-23 00:57:54 -0500

    add multiprovider (#5)

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:cubit/pages/pagina1_page.dart';
 import 'package:cubit/pages/pagina2_page.dart';
+import 'package:cubit/services/usuario_service.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,17 +11,24 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (_) => UsuarioService(),
+        )
+      ],
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+        ),
+        initialRoute: 'pagina1',
+        routes: {
+          'pagina1': (_) => Pagina1Page(),
+          'pagina2': (_) => Pagina2Page(),
+        },
       ),
-      initialRoute: 'pagina1',
-      routes: {
-        'pagina1': (_) => Pagina1Page(),
-        'pagina2': (_) => Pagina2Page(),
-      },
     );
   }
 }
